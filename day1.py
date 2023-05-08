d3595fa9a5eb911592fa1affcef865e7d7b1503f
doubleDimple <lovele.cn@gmail.com>
2023-05-08 21:32:22 +0800

    python学习第一天

---
+++ day1.py
@@ -0,0 +1,56 @@
+
+print("Hello, World!")
+
+#缩进：Python 中的代码块使用缩进来表示。一般来说，建议使用 4 个空格作为缩进。
+if 10 > 5:
+    print("10 > 5")
+
+#变量：Python 中的变量不需要显式地声明。当你为一个变量赋值时，Python 会自动确定变量的数据类型。
+
+x = 10
+y = "hello"
+
+#数据类型：Python 中常用的数据类型包括整数、浮点数、字符串、布尔值等。
+x1 = 10
+y1 = 10000000000000000000000000000000000000000000000
+
+#浮点数：Python 中的浮点数可以表示小数。
+
+x2 = 3.14
+y2 = -0.0001
+
+#字符串：Python 中的字符串可以使用单引号或双引号表示。
+
+x3 = "hello"
+y3 = 'hello'
+
+#布尔值：Python 中的布尔值只有两个取值：True 和 False。
+
+x4 = True
+y4 = False
+
+#运算符：Python 中常用的运算符包括算术运算符、比较运算符、逻辑运算符等。
+x5 = 10
+y5 = 3
+
+print(x5 + y5) #加法
+print(x5 - y5) #减法
+print(x5 * y5) #乘法
+print(x5 / y5) #除法
+print(x5 % y5) #取模
+print(x5 ** y5) #幂运算
+print(x5 // y5) #整除
+
+#比较运算符：
+print(x5 > y5)   # 大于
+print(x5 < y5)   # 小于
+print(x5 == y5)  # 等于
+print(x5 != y5)  # 不等于
+print(x5 >= y5)  # 大于等于
+print(x5 <= y5)  # 小于等于
+
+#逻辑运算符：
+print(x4 and y4) # 逻辑与
+print(x4 or y4) #逻辑或
+print(not x4) #逻辑非
+
